1de390de67339247e4953ce66373db436d45799f
AliAsgharGill <aliasghar@algotx.ai>
2025-02-02 13:46:49 +0500

    feat: add user endpoints, auth endpoints, and Dockerfile setup

--- backend-design-match/app/main.py
+++ backend-design-match/app/main.py
@@ -1,10 +1,12 @@
 # app/main.py
+from api.endpoints.auth import router as auth_router
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Design Match API", version="1.0")
 
 # Enable CORS for frontend interaction
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Update this to restrict access in production
@@ -12,6 +14,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.include_router(auth_router, prefix="/auth", tags=["auth"])
 
 
 @app.get("/")

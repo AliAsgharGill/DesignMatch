084135f5ff392fd8d93fdf70fc47717015d5b819
AliAsgharGill <stylinalivlogs@gmail.com>
2025-02-08 16:45:19 -0500

    fix: remove unused imports and update home component

--- src/containers/home/index.tsx
+++ src/containers/home/index.tsx
@@ -1,7 +1,3 @@
-import Carousel from "@/src/components/carousel/carousel";
-import Hero from "@/src/components/hero";
-import { workWithUsData } from "@/src/components/home/data";
-import WorkWithUsSection from "@/src/components/home/work-with-us";
 
 export default function Home() {
   return (
@@ -9,13 +5,10 @@ export default function Home() {
       <div className="container mx-auto px-6 w-full">
         <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start w-full">
           <div className="flex flex-col items-center justify-center w-full h-full">
-            <Hero />
+            <h1>Home</h1>
           </div>
         </main>
       </div>
-      {/* <div className="absolute bg-custom-svg bg-cover -mt-10 w-full h-full" /> */}
-      <Carousel />
-      <WorkWithUsSection data={workWithUsData} />
     </div>
   );
 }
